218986af56ea26c6da1512b4a704c0968cc408ac
sanaobul <35870474+sanaobul@users.noreply.github.com>
2020-06-02 17:50:54 -0500

    new chnage

---
+++ notebooks/MySamples/Test.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+#Creating widgets for leveraging parameters, and printing the parameters
+dbutils.widgets.text("input", "","")
+y = dbutils.widgets.get("input")
+print ("Param -\'input':")
+print (y)
+
+
+# COMMAND ----------
+
+# Creating widgets for leveraging parameters, and printing the parameters
+#def myfunc():
+#  dbutils.widgets.text("input", "","")
+#  y = dbutils.widgets.get("input")
+#  print ("Param -\'input':")
+#  print (y)
+#
+#while True:
+#  myfunc()
